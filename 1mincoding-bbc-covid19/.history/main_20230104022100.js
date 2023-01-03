4d8762c5615cae934e09051d2be2183787b4c1e9
SeungYn <kwls0407@gmail.com>
2023-01-04 03:15:59 +0900

    스크롤 장난감

---
+++ 1mincoding-bbc-covid19/.history/main_20230104022100.js
@@ -0,0 +1,30 @@
+(() => {
+  const stepElems = document.querySelectorAll('.step');
+  const graphicElems = document.querySelectorAll('.graphic-item');
+  let currentItem = graphicElems[0]; //현재 활성화된(visible 클래스가 붙은)
+  for (let i = 0; i < stepElems.length; i++) {
+    //stepElems[i].setAttribute('data-index', i);
+    stepElems[i].dataset.index = i;
+    graphicElems[i].dataset.index = i;
+  }
+
+  window.addEventListener('scroll', () => {
+    let step;
+    let boundingRect;
+    for (let i = 0; i < stepElems.length; i++) {
+      step = stepElems[i];
+      boundingRect = step.getBoundingClientRect();
+
+      if (
+        boundingRect.top > window.innerHeight * 0.1 &&
+        boundingRect.top < window.innerHeight * 0.8
+      ) {
+        if (currentItem) {
+          currentItem.classList.remove('visible');
+        }
+        currentItem = graphicElems[step.dataset.index];
+        currentItem.classList.add('visible');
+      }
+    }
+  });
+})();
