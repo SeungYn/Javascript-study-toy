fa07eda5acb8f89f6ddcd8704f707cf91e5450ad
SeungYn <kwls0407@gmail.com>
2024-05-30 09:49:24 +0900

    애플: 특정 타이밍 스크롤 애니메이션 기능 추가

--- apple-clone-v11/js/my.js
+++ apple-clone-v11/js/my.js
@@ -2,6 +2,8 @@
   let yOffset = 0; // window.pageYOffset 대신 쓸 변수
   let prevScrollHeight = 0; //현재 스크롤 위치 보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
   let currentScene = 0; //현재 활성화된 씬
+  let enterNewScene = false; // 새로운 씬이 시작된 순간
+
   //4개의 스크롤 구간에 대한 객체배열
   const sceneInfo = [
     {
@@ -11,6 +13,15 @@
       scrollHeight: 0,
       objs: {
         container: document.querySelector('#scroll-section-0'),
+        messageA: document.querySelector('#scroll-section-0 .main-message.a'),
+        messageB: document.querySelector('#scroll-section-0 .main-message.b'),
+        messageC: document.querySelector('#scroll-section-0 .main-message.c'),
+        messageD: document.querySelector('#scroll-section-0 .main-message.d'),
+      },
+      values: {
+        // 변화될 css값들 start, end는 애니메이션 적용될 구간
+        messageA_opacity: [0, 1, { start: 0.1, end: 0.2 }],
+        messageB_opacity: [0, 1, { start: 0.3, end: 0.4 }],
       },
     },
     {
@@ -65,7 +76,68 @@
     document.body.setAttribute('id', `show-scene-${currentScene}`);
   }
 
+  /**
+   *
+   * @param {*} values 변화될 oppacitry
+   * @param {*} currentYOffset  현재 씬에서 스크롤된 값
+   */
+  function calcValues(values, currentYOffset) {
+    let rv;
+    // 현재 씬 스크롤 비율을 나타냄
+    let scrollRatio = currentYOffset / sceneInfo[currentScene].scrollHeight;
+    const scrollHeight = sceneInfo[currentScene].scrollHeight;
+
+    if (values.length >= 3) {
+      // start ~ end 사이에 애니메이션 실행
+      const partScrollStart = values[2].start * scrollHeight;
+      const partScrollEnd = values[2].end * scrollHeight;
+      const partScrollHeight = partScrollEnd - partScrollStart;
+
+      if (
+        currentYOffset >= partScrollStart &&
+        currentYOffset <= partScrollEnd
+      ) {
+        rv =
+          ((currentYOffset - partScrollStart) / partScrollHeight) *
+            (values[1] - values[0]) +
+          values[0];
+      } else if (currentYOffset < partScrollStart) {
+        rv = values[0];
+      } else if (currentYOffset > partScrollEnd) {
+        rv = values[1];
+      }
+    } else {
+      rv = scrollRatio * (values[1] - values[0]) + values[0];
+    }
+
+    return rv;
+  }
+
+  function playAnimation() {
+    const objs = sceneInfo[currentScene].objs;
+    const values = sceneInfo[currentScene].values;
+    const currentYOffset = yOffset - prevScrollHeight;
+
+    // 현재 씬만 애니메이션 되도록 해주는 함수
+    switch (currentScene) {
+      case 0:
+        let messageA_opacity_in = calcValues(
+          values.messageA_opacity,
+          currentYOffset
+        );
+        objs.messageA.style.opacity = messageA_opacity_in;
+        break;
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+    }
+  }
+
   function scrollLoop() {
+    enterNewScene = false;
     prevScrollHeight = 0;
     //currentScene 으로 현재 내가 보고있는 씬에서 이전의 씬들의 값을 구해줌 2번쨰 씬을 보고있으면 1번째 씬의 값이 할당됨
     for (let i = 0; i < currentScene; i++) {
@@ -74,19 +146,24 @@
     }
 
     if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+      enterNewScene = true;
       currentScene++;
       document.body.setAttribute('id', `show-scene-${currentScene}`);
     }
 
     if (yOffset < prevScrollHeight) {
+      enterNewScene = true;
       if (currentScene === 0) return; //브라우져가 바운스효과가 일어나면 yOffset이 마이너스가 될 수 있음 그걸 방지
       currentScene--;
       document.body.setAttribute('id', `show-scene-${currentScene}`);
     }
+
+    if (enterNewScene) return; // 씬이 바뀌는 순간 calcValues가 음수값이 나오는 것을 방지하기 위해 종료
+    playAnimation();
   }
 
   window.addEventListener('scroll', () => {
-    console.log('scroll');
+    //console.log('scroll');
     yOffset = window.pageYOffset;
     scrollLoop();
   });
